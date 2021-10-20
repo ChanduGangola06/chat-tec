e8555d4e6470115c7f3abc6293315a129f8be083
ChanduGangola06 <chandugangola444@gmail.com>
2021-10-20 08:23:32 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBy86ZSFs8tpmhdGFMzEhUjgUpULs63KvA',
+  authDomain: 'chat-tec-9132c.firebaseapp.com',
+  databaseURL:
+    'https://chat-tec-9132c-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'chat-tec-9132c',
+  storageBucket: 'chat-tec-9132c.appspot.com',
+  messagingSenderId: '969081644110',
+  appId: '1:969081644110:web:b038de6ea2494c2d50cf1f',
+  measurementId: 'G-YY3TZJ1M48',
+};
+
+const app = firebase.initializeApp(config);
