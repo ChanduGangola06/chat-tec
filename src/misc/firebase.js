9178a0c3b963fb72f00da529bb236254834a103b
ChanduGangola06 <chandugangola444@gmail.com>
2021-10-21 08:39:43 +0530

    Added sign in page and sign in with facebook/google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyBy86ZSFs8tpmhdGFMzEhUjgUpULs63KvA',
@@ -13,3 +15,7 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+
+export const database = app.database();
